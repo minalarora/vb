677099dd9fa3d256d354789caf57522ce55fc3bd
i_feel_code <32245119+minalarora@users.noreply.github.com>
2021-05-08 16:49:37 +0530

    DS

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const server = http.createServer(app)
 // const io = socketio(server)
 
 
-const port= process.env.PORT || 80
+const port= process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'));
 
